895f03b293beb5b75fbe2f346f9171f3c7c6f53b
unknown <david.frugier@gmail.com>
2019-06-03 00:58:45 +0200

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use App\Entity\Article;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    // indique le nom de la route grace à l'annotation(@)
+    /** 
+     * @route("/", name="home") 
+     */
+
+     // render : fichier twig a afficher
+    public function home() {
+       return $this->render('blog/home.html.twig', [
+           'title' => "Les Rois Du Gato"
+           ]);
+    }
+
+     /** 
+     * @route("/recettes", name="recettes") 
+     */
+    public function allRecipe()
+    {
+       // recupère les champs de la table
+       $repo = $this->getDoctrine()->getRepository(Article::class);
+
+      $articles = $repo->findAll();
+
+        return $this->render('blog/allRecipe.html.twig', [
+         'title' => "Toutes les recettes", 'articles' => $articles
+        ]);
+
+        }
+          /** 
+     * @route("/recette/1", name="recette") 
+     */
+ function showRecipe()
+ {
+
+   $repo = $this->getDoctrine()->getRepository(Article::class);
+
+   $articles = $repo->findAll();
+
+     return $this->render('blog/showRecipe.html.twig', [
+      'articles' => $articles
+     ]);
+ }
+
+          /** 
+     * @route("/new", name="new") 
+     */
+ function create(Request $request, ObjectManager $manager)
+ {
+
+   $article = new Article();
+
+   $form = $this->createFormBuilder($article)
+                        ->add('titre')
+                        ->add('difficulte')
+                        ->add('createdAt')
+                        ->add('modifiedAt')
+                        ->add('image')
+                        ->add('ingredients')
+                        ->add('preparation')
+                        ->add('cuisson')
+                        ->add('recette')
+                        ->getForm();
+
+    $form->handleRequest($request);
+
+   if($form->isSubmitted() && $form->isValid()) {
+         
+
+         $manager->persist($article);
+         $manager->flush();
+
+   }
+ 
+     return $this->render('blog/create.html.twig', [
+      'title' => "Nouvelle Recette" , 'form' => $form->createView()
+     ]);
+ }
+
+ 
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $difficulte;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modifiedAt;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $ingredients;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $preparation;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $cuisson;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $recette;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDifficulte(): ?string
+    {
+        return $this->difficulte;
+    }
+
+    public function setDifficulte(string $difficulte): self
+    {
+        $this->difficulte = $difficulte;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->modifiedAt;
+    }
+
+    public function setModifiedAt(\DateTimeInterface $modifiedAt): self
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getIngredients(): ?string
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(string $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getPreparation(): ?string
+    {
+        return $this->preparation;
+    }
+
+    public function setPreparation(string $preparation): self
+    {
+        $this->preparation = $preparation;
+
+        return $this;
+    }
+
+    public function getCuisson(): ?string
+    {
+        return $this->cuisson;
+    }
+
+    public function setCuisson(?string $cuisson): self
+    {
+        $this->cuisson = $cuisson;
+
+        return $this;
+    }
+
+    public function getRecette(): ?string
+    {
+        return $this->recette;
+    }
+
+    public function setRecette(string $recette): self
+    {
+        $this->recette = $recette;
+
+        return $this;
+    }
+}
